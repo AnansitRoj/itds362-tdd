56b6e1f18c4a458bece8d0f09876b72f6f33646f
Anansit <Anansit.roj@student.mahidol.edu>
2026-09-03 12:16:30 +0700

    A3: value object, green

--- test_kitchen.py
+++ test_kitchen.py
@@ -2,7 +2,7 @@
 
 # รายการทดสอบ (test list)
 # [x] 200 g × 3 = 600 g
-# [ ] การคูณต้องไม่เปลี่ยนค่าของอ็อบเจ็กต์เดิม
+# [x] การคูณต้องไม่เปลี่ยนค่าของอ็อบเจ็กต์เดิม
 # [ ] ปริมาณสองค่าที่มีทั้งตัวเลขและหน่วยเท่ากันถือว่าเท่ากัน
 # [ ] 1 oz ไม่เท่ากับ 1 g
 # [ ] 200 g + 300 g = 500 g
@@ -14,14 +14,13 @@ from kitchen import Quantity
 
 def test_multiplication():
     flour = Quantity(200)
-    flour.times(3)
-    assert flour.amount == 600
+    assert flour.times(3).amount == 600
 
 
 def test_multiplication_by_two():
     flour = Quantity(200)
-    flour.times(2)
-    assert flour.amount == 400
+    assert flour.times(2).amount == 400
+
 
 def test_multiplication_returns_a_new_quantity():
     flour = Quantity(200)
