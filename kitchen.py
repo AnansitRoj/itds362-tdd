4de44429d8319756bcbc28adebd54c7656d87e5a
Anansit <Anansit.roj@student.mahidol.edu>
2026-09-03 12:48:05 +0700

    A6: multiply after addition, green

--- kitchen.py
+++ kitchen.py
@@ -1,4 +1,19 @@
 # kitchen.py
+class Converter:
+    RATES = {
+        ("oz", "g"): 28.3495,
+    }
+
+    def reduce(self, quantity_or_sum, unit):
+        return quantity_or_sum.reduce(unit, self)
+
+    def convert(self, amount, from_unit, to_unit):
+        if from_unit == to_unit:
+            return amount
+        rate = self.RATES[(from_unit, to_unit)]
+        return amount * rate
+
+
 class Quantity:
     def __init__(self, amount, unit):
         self.amount = amount
@@ -10,8 +25,9 @@ class Quantity:
     def plus(self, other):
         return Sum(self, other)
 
-    def reduce(self, unit):
-        return self
+    def reduce(self, unit, converter):
+        converted = converter.convert(self.amount, self.unit, unit)
+        return Quantity(converted, unit)
 
     def __eq__(self, other):
         return self.amount == other.amount and self.unit == other.unit
@@ -25,11 +41,10 @@ class Sum:
         self.left = left
         self.right = right
 
-    def reduce(self, unit):
-        amount = self.left.amount + self.right.amount
-        return Quantity(amount, unit)
+    def times(self, multiplier):
+        return Sum(self.left.times(multiplier), self.right.times(multiplier))
 
-
-class Converter:
-    def reduce(self, quantity_or_sum, unit):
-        return quantity_or_sum.reduce(unit)
+    def reduce(self, unit, converter):
+        left_amount = converter.convert(self.left.amount, self.left.unit, unit)
+        right_amount = converter.convert(self.right.amount, self.right.unit, unit)
+        return Quantity(left_amount + right_amount, unit)
